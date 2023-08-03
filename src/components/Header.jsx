b9b3b39a414ff97916b6c9094b84b4ca803d88ed
Sushil Dangi <dangi.sushil5@gmail.com>
2023-08-03 19:11:13 +0530

    sub domain to folder

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -20,7 +20,7 @@ const Header = () => {
             <a href="/#about">About</a>
           </li>
           <li>
-            <a href="/blog/">Contact</a>
+            <a href="https://blog.sushildangi.online">My Blogs</a>
           </li>
           <li>
             <a href="/#contact">Contact</a>
